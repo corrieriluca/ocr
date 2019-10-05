aef0570aa7fa49e65c381795183f9838fcbc41a5
alexis boissiere <alexis.boissiere@epita.fr>
2019-10-05 20:19:53 +0200

    Add sigmoid function

--- src/neural/math_tools.c
+++ src/neural/math_tools.c
@@ -1,3 +1,7 @@
 #include <stdio.h>
 #include "backpropagation.h"
 
+double sigmoid(double val)
+{
+    return 1.0/(1.0 + exp(-val));
+}
--- src/neural/math_tools.h
+++ src/neural/math_tools.h
@@ -1,7 +1,7 @@
 #ifndef MATH_TOOLS_H
 #define MATH_TOOLS_H
 
-
+double sigmoid(double val);
 
 #endif
 
