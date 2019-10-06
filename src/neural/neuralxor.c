83edabe2da662861c06c378463513ed869ba5623
alexis boissiere <alexis.boissiere@epita.fr>
2019-10-06 17:00:44 +0200

    Add beginning of xor neural network

--- src/neural/neuralxor.c
+++ src/neural/neuralxor.c
@@ -5,27 +5,44 @@
 
 int main()
 {
-        double mat1[] = {1,2,3,4};
-        double mat2[] = {5,6,7,8};
-   double output[2*2];
-        multiply_matrix(mat1,mat2,output,2,2,2,2);
-
-        double input[2*2];
-        init_matrix_random(input, 2, 2);
-
+	//for a XOR with a hidden layer of 3 neurons
+	double input[] = {0,1};
+	double weight1[2*3];
+	double weight2[3*1];
+	init_matrix_random(weight1, 2, 3);
+	init_matrix_random(weight2, 2, 3);
 
 
-        for (size_t i = 0; i < 2*2; ++i)
-        {
-                printf("%lf\n", output[i]);
-        }
 
-        for (size_t b = 0; b < 2*2; ++b)
-        {
-                printf("%lf\n", input[b]);
-        }
-        return 0;
+
+	//multiply_matrix(mat1,mat2,output,2,2,2,2);
+	//init_matrix_random(input, 2, 2);
+	
+	return 0;
 }
+
+double feedforward(double *input, double *weight1)
+{
+	double outputf[] = {0,0,0};
+
+	multiply_matrix(input, weight1, outputf, 1, 1, 2, 3);
+	//sigmoid(outputf);
+
+	return *outputf;
+}
+
+
+
+void printf_array(double *a)
+{
+	size_t n = sizeof(a)/sizeof(a[0]);
+
+	for (size_t i = 0; i < n; ++i)
+	{
+			 printf("%lf\n", a[i]);
+	}
+}
+
 
 
 
