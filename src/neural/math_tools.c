077b4e71af99bc989ee66c772727fcd923ce16ff
alexis boissiere <alexis.boissiere@epita.fr>
2019-10-06 10:41:26 +0200

    Add sigmoid prime

--- src/neural/math_tools.c
+++ src/neural/math_tools.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "backpropagation.h"
 
-double sigmoid(double val)
+double sigmoid(double z)
 {
-    return 1.0/(1.0 + exp(-val));
+	return (double)(1.0 / (1.0 + exp(-z)));
 }
+
+double sigmoid_prime(double z)
+{
+	return z * (1 - z);
+}
+
+
+//Hadamard product
+// Equation for the error in the output layer
+//
