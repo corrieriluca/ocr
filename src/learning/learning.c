a0c47a9118b79a5dbdcb636722ad5ee082881c33
Luca Corrieri <luca.corrieri@epita.fr>
2019-10-19 13:00:06 +0200

    Added learning.c for the learning process

---
+++ src/learning/learning.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include "../image_operations.h"
+#include "../preprocessing.h"
+#include "../segmentation.h"
+
+int main()
+{   
+    SDL_Surface *character_surface;
+    init_sdl();
+    char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    
+    Character characters[52];
+
+    printf("-------------- LEARNING PROCESS -----------------\n\n");
+    printf("--- LOADING CHARACTERS ---\n");
+    
+    for (size_t i = 0; i < 52; i++)
+    {
+        printf("Loading letters/%zu.png => It's a '%c'\n", i, letters[i]);
+        char path[15];
+        snprintf(path, 15, "letters/%zu.png", i);
+
+        character_surface = load_image(path);
+        size_t width = character_surface->w; // 16
+        size_t height = character_surface->h; // 16
+
+        binarize(character_surface);
+        size_t *bin_matrix = calloc(width * height, sizeof(size_t));
+        image_to_bin_matrix(character_surface, bin_matrix);
+        
+        double char_mat[width * height];
+        for (size_t j = 0; j < width * height; j++)
+            char_mat[j] = (double)bin_matrix[j];
+
+        free(bin_matrix);
+
+        // creating the character object;
+        Character character;
+        character.startingPoint = 0;
+        character.endPoint = 0;
+        character.matrix = char_mat;
+        character.character = letters[i];
+        // adding the character to the array of characters
+        characters[i] = character;
+    }
+
+    SDL_FreeSurface(character_surface);
+
+    // ----- HERE ARE THE CALLS TO FUNCTIONS FROM THE NN FOR TRAINING IT -------
+
+    return 0;
+}
--- src/preprocessing.h
+++ src/preprocessing.h
@@ -1,6 +1,8 @@
 #ifndef PREPROCESSING_H
 #define PREPROCESSING_H
 
+void binarize(SDL_Surface *image_surface);
+void image_to_bin_matrix(SDL_Surface *img_surface, size_t *bin_matrix);
 size_t *image_to_matrix(SDL_Surface *image_surface);
 
 #endif
