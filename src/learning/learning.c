61346d738f54c48820346b6df49ddb87f9a598f4
Luca Corrieri <luca.corrieri@epita.fr>
2019-10-21 18:32:23 +0200

    Better letter samples in Arial in 28*28px + Better learning.c
    The 'letters/' folder has been renamed to 'arial/' because other fonts
    will be added later...

--- src/learning/learning.c
+++ src/learning/learning.c
@@ -4,35 +4,43 @@
 #include "../image_operations.h"
 #include "../preprocessing.h"
 #include "../segmentation.h"
+#include "../matrix_tools.h"
 
-int main()
-{   
+#define CHARACTER_NUMBER 67
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+        errx(1, "Error: you must specify the folder of the samples");
+
+    // Check if the argument is a folder
+    if (argv[1][strlen(argv[1]) - 1] != '/')
+        errx(2, "Error: you must add a '/' after the folder name");
+
     SDL_Surface *character_surface;
     init_sdl();
-    char letters[] = 
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,!?'";
-    
-    Character characters[57];
+    char letters[] =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,!?'0123456789";
+
+    Character characters[CHARACTER_NUMBER];
 
     printf("-------------- LEARNING PROCESS -----------------\n\n");
     printf("--- LOADING CHARACTERS ---\n");
-    
-    for (size_t i = 0; i < 57; i++)
+
+    for (size_t i = 0; i < CHARACTER_NUMBER; i++)
     {
-        printf("Loading letters/%zu.png => It's a '%c'\n", i, letters[i]);
+        printf("Loading %s%zu.png => It's a '%c'\n", argv[1], i, letters[i]);
         char path[15];
-        snprintf(path, 15, "letters/%zu.png", i);
+        snprintf(path, 15, "%s%zu.png", argv[1], i);
 
         character_surface = load_image(path);
-        size_t width = character_surface->w; // 16
-        size_t height = character_surface->h; // 16
 
         binarize(character_surface);
-        size_t *bin_matrix = calloc(width * height, sizeof(size_t));
+        size_t *bin_matrix = calloc(MATRIX_SIZE * MATRIX_SIZE, sizeof(size_t));
         image_to_bin_matrix(character_surface, bin_matrix);
-        
-        double *char_mat = calloc(width * height, sizeof(double));
-        for (size_t j = 0; j < width * height; j++)
+
+        double *char_mat = calloc(MATRIX_SIZE * MATRIX_SIZE, sizeof(double));
+        for (size_t j = 0; j < MATRIX_SIZE * MATRIX_SIZE; j++)
             char_mat[j] = (double)bin_matrix[j];
 
         free(bin_matrix);
@@ -49,12 +57,12 @@ int main()
 
     SDL_FreeSurface(character_surface);
 
-    printf("\nDEBUGGIN Characters array = [");
-    for (size_t i = 0; i < 57; i++)
+    printf("\nDEBUGGING Characters array = [");
+    for (size_t i = 0; i < CHARACTER_NUMBER; i++)
         printf("%c", characters[i].character);
     printf("]\n");
 
-    // ----- HERE ARE THE CALLS TO FUNCTIONS FROM THE NN FOR TRAINING IT -------
+    // ----- HERE ARE CALLED TO FUNCTIONS FROM THE NN FOR TRAINING IT -------
 
     printf("\n--- SENDING CHARACTERS TO THE NEURAL NETWORK ---\n");
 
