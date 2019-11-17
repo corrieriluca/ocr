b203df3f7d3f044e547a9faca8b012cf59015365
alexis boissiere <alexis.boissiere@epita.fr>
2019-11-18 00:57:41 +0100

    [Update] test_neuralxor.sh to save file to .ocr

--- src/neural/Xor_neural_network/test_neuralxor.sh
+++ src/neural/Xor_neural_network/test_neuralxor.sh
@@ -1,39 +1,39 @@
 make all
 
-if [ -f test_neuralxor.txt ]; then
-	rm test_neuralxor.txt
+if [ -f test_neuralxor.ocr ]; then
+	rm test_neuralxor.ocr
 fi
 
 
 printf "Test with 5 neurons in hidden layer and 100000 epoch\n"
-printf "\e[96mTest with 5 neurons in hidden layer and 100000 epoch\n\e[0m" >> test_neuralxor.txt
+printf "\e[96mTest with 5 neurons in hidden layer and 100000 epoch\n\e[0m" >> test_neuralxor.ocr
 for i in {0..15}; do 
-	printf "\e[32m$(./neural.out 5 100000)\e[0m\n" >> test_neuralxor.txt
+	printf "\e[32m$(./neural.out 5 100000)\e[0m\n" >> test_neuralxor.ocr
 	printf "$i/15 "
 done
 
 printf "\nTest with 10 neurons in hidden layer and 100000 epoch\n"
-printf "\e[96mTest with 10 neurons in hidden layer and 100000 epoch\n\e[0m" >> test_neuralxor.txt
+printf "\e[96mTest with 10 neurons in hidden layer and 100000 epoch\n\e[0m" >> test_neuralxor.ocr
 for i in {0..15}; do 
-	printf "\e[32m$(./neural.out 10 100000)\e[0m\n" >> test_neuralxor.txt
+	printf "\e[32m$(./neural.out 10 100000)\e[0m\n" >> test_neuralxor.ocr
 	printf "$i/15 "
 done
 
 printf "\nTest with 20 neurons in hidden layer and 100000 epoch\n"
-printf "\e[96mTest with 20 neurons in hidden layer and 100000 epoch\n\e[0m" >> test_neuralxor.txt
+printf "\e[96mTest with 20 neurons in hidden layer and 100000 epoch\n\e[0m" >> test_neuralxor.ocr
 for i in {0..15}; do 
-	printf "\e[32m$(./neural.out 20 100000)\e[0m\n" >> test_neuralxor.txt
+	printf "\e[32m$(./neural.out 20 100000)\e[0m\n" >> test_neuralxor.ocr
 	printf "$i/15 "
 done
 
 
 printf "\nTest with 50 neurons in hidden layer and 100000 epoch\n"
-printf "\e[96mTest with 50 neurons in hidden layer and 100000 epoch\n\e[0m" >> test_neuralxor.txt
+printf "\e[96mTest with 50 neurons in hidden layer and 100000 epoch\n\e[0m" >> test_neuralxor.ocr
 for i in {0..15}; do 
-	printf "\e[32m$(./neural.out 50 100000)\e[0m\n" >> test_neuralxor.txt
+	printf "\e[32m$(./neural.out 50 100000)\e[0m\n" >> test_neuralxor.ocr
 	printf "$i/15 "
 done
 
 
 clear
-cat test_neuralxor.txt
+cat test_neuralxor.ocr
