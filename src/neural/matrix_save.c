37a0f72e4793c9ee5eece29920d0aa900e47ad52
alexis boissiere <alexis.boissiere@epita.fr>
2019-11-17 16:44:28 +0100

    [Add] init_a0

--- src/neural/matrix_save.c
+++ src/neural/matrix_save.c
@@ -81,7 +81,7 @@ int mat_to_file(int mat1[], int mat2[], int mat3[], int mat4[], double mat5[],
 
 	return 0;
 }
-
+/*
 int main()                                                                      
 {
 	int a[2] = {3,3};
@@ -102,4 +102,4 @@ int main()
 	{
 		return 1;
 	}                                                                     
-}
+}*/
