ce2b48b0d7b5cd7cbb9736ef0e024e2b946f934b
Luca Corrieri <luca.corrieri@epita.fr>
2019-11-20 18:20:24 +0100

    (GUI) get the path from selecting a image

--- src/gui.c
+++ src/gui.c
@@ -1,8 +1,9 @@
 #include <gtk/gtk.h>
 
-
 GtkWidget *window;
 GtkWidget *window_main;
+
+GtkWidget *fcb_image;
 
 int open_main_win = 0;
 
@@ -16,6 +17,9 @@ void launch_gui(int argc, char *argv[])
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window_start"));
     window_main = GTK_WIDGET(gtk_builder_get_object(builder, "window_main_menu"));
+
+    // file chooser button in window_main_menu
+    fcb_image = GTK_WIDGET(gtk_builder_get_object(builder, "fcb_image"));
 
     gtk_builder_connect_signals(builder, NULL);
     
@@ -32,6 +36,7 @@ void on_window_start_destroy()
     	gtk_main_quit();
 }
 
+// called when main window is closed
 void on_window_main_menu_destroy()
 {
 	gtk_main_quit();
@@ -46,8 +51,26 @@ void on_btn_start_app_clicked()
 }
 
 // called when the user quits the app from the
-// start window
-void on_btn_quit_start_app_clicked()
+// - start window
+// - the main window (menu bar)
+// - the main window (quit button)
+void on_btn_quit_clicked()
 {
     gtk_main_quit();
+}
+
+// called when a file is selected with the file chooser button
+void on_fcb_image_file_set()
+{
+    char *filename;
+    GtkFileChooser *chooser = GTK_FILE_CHOOSER(fcb_image);
+    filename = gtk_file_chooser_get_filename(chooser);
+    printf("\nGTK Debug : file selected is %s\n", filename);
+    g_free(filename);
+}
+
+// called when the convert button is clicked (calls the main function of the OCR)
+void on_btn_convert_clicked()
+{
+    printf("\nGTK Debug : btn_convert clicked\n");
 }
