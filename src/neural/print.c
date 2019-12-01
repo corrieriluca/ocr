6483ec65ac946a5100d7cd20c265659c8e3320da
alexis boissiere <alexis.boissiere@epita.fr>
2019-12-01 14:06:46 +0100

    [Change] number of print test

--- src/neural/print.c
+++ src/neural/print.c
@@ -100,7 +100,7 @@ void print_feed_forward(double *weight0, double *weight1, double *a0,
 	}
 
 	double compteur = 0.0;
-	int nb_test = 60;
+	int nb_test = 52;
 
 	for (int i = 0; i < nb_test; i++)
 	{
