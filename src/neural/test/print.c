85fb3dba43597f2fad050f0636442f703d322971
alexis boissiere <alexis.boissiere@epita.fr>
2019-12-07 21:50:41 +0100

    [Remove] unused function

--- src/neural/test/print.c
+++ src/neural/test/print.c
@@ -4,81 +4,6 @@
 #include "neural.h"
 #include "math_tools.h"
 
-
-void pretty_print_xor(size_t nb_char, char *str)
-{
-	if (nb_char < strlen(str)+6)
-	{
-		printf("pretty_print_xor: Can't print with these imputs");
-	}
-
-	else
-	{
-		size_t i;
-		size_t middle = nb_char / 2 - strlen(str) / 2 - 3;
-
-		for (i = 0; i < nb_char; i++)
-		{
-			printf("#");
-		}
-
-		printf("\n###");
-
-
-		for (i = 0; i < nb_char - strlen(str) - 5; i++)
-		{
-			if (i == middle)
-			{
-				printf("%s",str);
-			}
-			else
-			{
-				printf(" ");
-			}
-		}
-		printf("###\n");
-
-		for (i = 0; i < nb_char; i++)
-		{
-			printf("#");
-		}
-
-		printf("\n");
-	}
-}
-
-
-void print_matrix_double(double matrix[], int size[])
-{
-  int width = size[1];
-  int height = size[0];
-
-  printf("*");
-  for (int i = 0; i < width; i++)
-  {
-    printf("-");
-  }
-   printf("*\n");
-
-   for (int j = 0; j < height; j++)
-   {
-   printf("|");
-   for (int k = 0; k < width; k++)
-   {
-       printf("%f ", matrix[k + j * width]);
-    }
-    printf("|\n");
-  }
-
-  printf("*");
-  for (int l = 0; l < width; l++)
-  {
-    printf("-");
-  }
-  printf("*\n");
-}
-
-
 void print_feed_forward(double *weight0, double *weight1, double *a0,
 		double *a1, double *a2, double *b0, double *b1, int *size_w0,
 		int *size_w1, int *size_a0, int *size_a1, int *size_a2,
@@ -108,7 +33,7 @@ void print_feed_forward(double *weight0, double *weight1, double *a0,
 		fgets(charac, sizeof(charac), char_db_test);
 		char good_char = charac[0];
 
-		char matrix[(size_a0[0]*size_a0[1]*2+2)]; //TODO : Find good size
+		char matrix[(size_a0[0]*size_a0[1]*2+2)];
 		fgets(matrix, (size_a0[0]*size_a0[1]*2+2), matrix_db_test);
 
 		int size = size_a0[0] * size_a0[1];
@@ -145,20 +70,6 @@ void print_feed_forward(double *weight0, double *weight1, double *a0,
 
 		printf("Given letter : %c (index = %02d) || ", good_char, return_char_index);
 		printf("output letter : %c (index = %02d)\033[0m\n", return_char, index_max_matrix(a2,size_a2));
-
-
-		/*//Ugly but usefull to print a2
-		for (int z = 0; z < (size_a2[0] * size_a2[1]); z++)
-		{
-			if ((z % 10) == 0 && (z != 0))
-			{
-				printf("\n");
-			}
-
-			printf("|%lf|",a2[z]);
-		}
-
-		printf("\n");*/
 	}
 
 	double success_rate = compteur / nb_test;
