acf9fc8d6b3969d9e3e37c9efa403d2c510f4480
alexis boissiere <alexis.boissiere@epita.fr>
2019-12-08 21:16:59 +0100

    [Add] maybe working neural

--- src/neural/neural.c
+++ src/neural/neural.c
@@ -25,7 +25,7 @@ void backpropagation(double *weight0, double *weight1, double *b0, double *b1,
 		double *a0, double *a1,	double *a2, int *size_w0, int *size_w1,
 		int *size_b0, int *size_b1, int *size_a0, int *size_a1, int size_a2[],
 		double *d_b1, double *d_w1, double *d_b0, double *d_w0,	int *s_d_b1, 
-		int *s_d_w1, int *s_d_b0, int *s_d_w0, char *good_char, double *sum_cost)
+		int *s_d_w1, int *s_d_b0, int *s_d_w0, char good_char, double *sum_cost)
 {
 	//feedforward to set a2 for backpro
 	multiply_matrix(weight0, a0, a1, size_w0, size_a0);
