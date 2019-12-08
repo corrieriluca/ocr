b14ec4ac4a2488eece16969212df21d3fedc3170
alexis boissiere <alexis.boissiere@epita.fr>
2019-12-08 13:18:16 +0100

    [ADD] recognize function

--- src/neural/test/print.c
+++ src/neural/test/print.c
@@ -4,6 +4,36 @@
 #include "neural.h"
 #include "math_tools.h"
 
+char recognize(double matchar[], double *weight0, double *weight1, double *a0,
+		double *a1, double *a2, double *b0, double *b1, int *size_w0,
+		int *size_w1, int *size_a0, int *size_a1, int *size_a2,
+		int *size_b0, int *size_b1)
+{
+
+	int size = size_a0[0] * size_a0[1];
+	for (int i = 0; i < size; i++)
+	{
+		if (matchar[i] == '0')
+		{
+			a0[i] = 0.0;
+		}
+		else
+		{
+			a0[i] = 1.0;
+		}
+	}
+
+	feedforward(weight0, weight1, a0, a1, a2, b0, b1, size_w0, size_w1, size_a0,
+		size_a1, size_a2, size_b0, size_b1);
+
+
+	char return_char = '*';
+	return_char = find_index_letter(a2, size_a2);
+
+	return return_char;
+}
+
+
 void print_feed_forward(double *weight0, double *weight1, double *a0,
 		double *a1, double *a2, double *b0, double *b1, int *size_w0,
 		int *size_w1, int *size_a0, int *size_a1, int *size_a2,
--- src/neural/test/print.h
+++ src/neural/test/print.h
@@ -1,9 +1,10 @@
 #ifndef PRINT_H
 #define PRINT_H
-void pretty_print_xor(size_t nb_char, char *str) ;
 
-void print_matrix_double(double matrix[], int size[]);
-
+char recognize(double matchar[], double *weight0, double *weight1, double *a0, 
+		double *a1, double *a2, double *b0, double *b1, int *size_w0,
+        int *size_w1, int *size_a0, int *size_a1, int *size_a2,
+        int *size_b0, int *size_b1);
 
 void print_feed_forward(double *weight0, double *weight1, double *a0,
 		double *a1, double *a2, double *b0, double *b1, int *size_w0,
